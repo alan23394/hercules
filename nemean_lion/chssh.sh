d03d71361112635164b416e7d0580c6df9beb801
Alan <alan@debian-hercules.debian.home.com>
2019-03-01 23:31:23 -0800

    Small tweak to match the end of the line

--- nemean_lion/chssh.sh
+++ nemean_lion/chssh.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PORT_STATUS=$(grep '^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]' /etc/ssh/sshd_config)
+PORT_STATUS=$(grep '^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]$' /etc/ssh/sshd_config)
 
 if [ -n "$PORT_STATUS" ]; then
-	su -c 'sed -i.bak "s/^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]/Port 2398/" /etc/ssh/sshd_config && systemctl restart ssh'
+	su -c 'sed -i.bak "s/^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]$/Port 2397/" /etc/ssh/sshd_config && systemctl restart ssh'
 else
-	su -c 'echo "Port 2398" >> /etc/ssh/sshd_config && systemctl restart ssh'
+	su -c 'echo "Port 2397" >> /etc/ssh/sshd_config && systemctl restart ssh'
 fi
 
