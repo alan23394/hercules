1410462b4583a78742fc3af05bcbfd8c6a93e614
Alan <alan@debian-hercules.debian.home.com>
2019-03-01 23:24:52 -0800

    Done with nemean lion
    My script will append a port line to the file if there isn't one
    already, and if there is, it changes it. Then it restarts the sshd
    service to apply the changes.

---
+++ nemean_lion/chssh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PORT_STATUS=$(grep '^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]' /etc/ssh/sshd_config)
+
+if [ -n "$PORT_STATUS" ]; then
+	su -c 'sed -i.bak "s/^Port [[:digit:]][[:digit:]][[:digit:]][[:digit:]]/Port 2398/" /etc/ssh/sshd_config && systemctl restart ssh'
+else
+	su -c 'echo "Port 2398" >> /etc/ssh/sshd_config && systemctl restart ssh'
+fi
+
